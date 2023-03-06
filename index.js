577002f55e78e762585d4ff2737d67c4272e0234
Daniel <pepelacon@gmail.com>
2023-03-06 15:57:31 -0500

    create fetch

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("index.js");
+const findBook = () => {
+    const inputField = document.getElementById('input_form');
+    inputField.addEventListener('submit', handleSubmit)
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    document.getElementById('book-container').innerHTML = "";
+    const inputField = document.getElementById('input_form');
+    fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`)
+    .then(response => response.json())
+    .then(library => console.log(library))
+    .catch(error => console.error(error));
+    }
