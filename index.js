e98a95e87b75a52b09ad2134012aeb3a61e5a168
Daniel <pepelacon@gmail.com>
2023-03-06 16:12:22 -0500

    fetch joke works

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const findBook = () => {
     const inputField = document.getElementById('input_form');
+    console.log(inputField);
     inputField.addEventListener('submit', handleSubmit)
 }
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    document.getElementById('book-container').innerHTML = "";
     const inputField = document.getElementById('input_form');
     fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`)
     .then(response => response.json())
     .then(library => console.log(library))
     .catch(error => console.error(error));
     }
+
+    document.addEventListener('DOMContentLoaded', findBook)
