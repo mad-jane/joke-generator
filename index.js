2adcb5e8a0e6f0298078a71d69aba510b72adf45
Daniel <pepelacon@gmail.com>
2023-03-07 16:14:27 -0500

    add PATCH and DELETE fetch

--- index.js
+++ index.js
@@ -7,21 +7,19 @@ const handleSubmit = () => {
 
 
 const renderJoke = (library) => {
-  
-    // const divJoke = document.querySelector("#existing-jokes")
-    
+      
     const spanJokes = document.querySelector('#jokes')
     spanJokes.innerText = library.joke
 
-    const likeBtn = document.createElement('button')
-        likeBtn.className = "like"
-        likeBtn.innerText = "Favorite"
+    const fwrBtn = document.createElement('button')
+        fwrBtn.className = "like"
+        fwrBtn.innerText = "Favorite"
 
     const disBtn = document.createElement('button')
         disBtn.className = "dislike"
         disBtn.innerText = "Bad Joke"  
 
-    spanJokes.append(likeBtn, disBtn)
+    spanJokes.append(fwrBtn, disBtn)
 
     const obj = {
         jokes: library.joke,
@@ -29,7 +27,7 @@ const renderJoke = (library) => {
         likes: '0'
     }
     
-    likeBtn.addEventListener('click', () => handleLikeClick(obj))
+    fwrBtn.addEventListener('click', () => handleLikeClick(obj))
     disBtn.addEventListener('click', () => handleSubmit())
 }
 
@@ -55,8 +53,15 @@ const handleDB = () => {
 }
 
 const renderData = (element) => {
+    //chosen element div
     const divJokes = document.querySelector("#db-jokes")
-    console.log(divJokes);
+
+    //! create div for each joke and assign new class
+    const divJokesContent = document.createElement('div')
+    divJokesContent.className = "jokes-content"
+
+
+    // * create paragraphs for each joke
     const parJokes = document.createElement('p')
         parJokes.innerText = element.jokes
 
@@ -65,9 +70,43 @@ const renderData = (element) => {
 
     const likes = document.createElement('p')
         likes.innerText = element.likes
+
+
+    //$ our number of likes
+    let countLikes = parseInt(likes.textContent)
     
-        console.log(parJokes, category, likes);
-    divJokes.append(parJokes, category, likes)
+
+    //create like button for each joke and update the number of likes using PATCH method
+    const likeBtn = document.createElement('button')
+    likeBtn.innerText = "Like"
+    likeBtn.addEventListener('click', () => {
+        fetch(`http://localhost:3000/jokes/${element.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type':'application/json',
+                },
+            body: JSON.stringify({likes: ++countLikes})
+        })
+        .then(res => res.json())
+        .then(data => {likes.innerText = data.likes})
+    })
+
+
+    // * create delete button for each joke and update the number of likes using DELETE method
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "Delete"
+    deleteBtn.addEventListener('click', () => {
+        fetch(`http://localhost:3000/jokes/${element.id}`, {method: "DELETE"})
+        .then(res => res.json())
+        .then(data => divJokesContent.remove())
+    })
+
+    // add all elements to our div for each joke
+    divJokesContent.append(parJokes, category, likes, likeBtn, deleteBtn)
+
+
+    // append whole div with all elements to global div
+    divJokes.append(divJokesContent)
 }
 
 
